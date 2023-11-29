00359bf4d141c9310867ea9fc4ab923f1c7082da
Galina Ufimtseva <g.s.ufimtseva@gmail.com>
2023-11-29 13:43:08 -0500

    lecture 1 (first java project)

---
+++ Lecture1_FirstJavaProgram/FirstJavaProject/src/HW_1_1.java
@@ -0,0 +1,13 @@
+public class HW_1_1 {
+
+    public static void main(String[] args) {
+        for (int i = 3; i >= 1; i--) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println(i);
+        }
+        System.out.println("Here we go!!!");
+    }
+
+}
